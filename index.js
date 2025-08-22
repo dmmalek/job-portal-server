9dd49ec7ea4f28ae250c6eda9dcc022fe532e246
Abdul Malek <abdulmalekmonir97@gmail.com>
2025-08-22 12:14:01 +0600

    get data find by id

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-
 app.get("/", (req, res) => {
   res.send("Job portal is runnning...");
 });
@@ -17,7 +15,7 @@ app.listen(port, () => {
   console.log(`Job portal start at ${port}`);
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uze3k1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,10 +31,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // all data find query
     const jobCollection = client.db("JobPortal").collection("jobs");
     app.get("/jobs", async (req, res) => {
       const jobsFind = await jobCollection.find().toArray();
       res.send(jobsFind);
+    });
+
+    // data find by id
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const getJobById = await jobCollection.findOne(filter);
+      res.send(getJobById);
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
